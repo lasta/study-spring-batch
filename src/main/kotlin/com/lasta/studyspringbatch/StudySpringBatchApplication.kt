5f436ebd00192a3054ee47c23a9a7976aa72c02e
Atsushi Koshikizawa <lastarrow21@gmail.com>
2019-05-19 17:24:42 +0900

    Initialize with spring initializr

---
+++ src/main/kotlin/com/lasta/studyspringbatch/StudySpringBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.lasta.studyspringbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringBatchApplication>(*args)
+}
